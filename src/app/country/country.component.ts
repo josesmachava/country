8bfc1927ff88117a00ab4e43a744b7288be8ce96
= <jose.s.machava@gmail.com>
2019-10-25 14:31:27 +0200

    move from inline template  to external template for clean code

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -3,11 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-country',
-  template: `
-  <h1>hi</h1>
-      <ul   *ngFor="let country of contries">
-      <li>{{ country.name }}
-  `,  
+  templateUrl:'/country.component.html',  
   styleUrls: ['./country.component.css']
 })
 export class CountryComponent implements OnInit {
