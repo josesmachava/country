4b994ad92592f162392fd05e10c387a00f6e7ad3
= <jose.s.machava@gmail.com>
2019-10-25 14:18:26 +0200

    update countries interface with required information

--- src/app/countries.ts
+++ src/app/countries.ts
@@ -1,4 +1,13 @@
 export interface ICountries{
     id:number,
     name:string,
+    capital:string,
+    region:string,
+    subregion:string,
+    population:number, 
+    area:number,
+    timezones:string
+    nativeName:string,
+    flag:string
+   
 }
